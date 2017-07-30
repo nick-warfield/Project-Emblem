d053aa0009accde29d092aca51d5de146c98204e
warfield9000 <warfield9000@gmail.com>
2017-07-29 20:52:39 -0700

    New Combat Menu
    I updated the combat menu. It's now real easy to see who is doing what
    damage and all that.
    
    Now, only the character details menu is left to update. Plus, there's UI
    scaling issued but I won't worry about that for now. Fullscreen gets
    around that problem.

--- Dankest Embl3m/Assets/scripts/Game/Utility/getCombatInfo.cs
+++ Dankest Embl3m/Assets/scripts/Game/Utility/getCombatInfo.cs
@@ -6,7 +6,8 @@ using UnityEngine.UI;
 public class getCombatInfo : MonoBehaviour
 {
     public GameObject EnabledView, DisabledView;
-    public Text[] Info;
+    public Text[] Info1, Info2;
+    public Image Bar1, Bar2;
     CombatManager.DamageRelated[] Stats = new CombatManager.DamageRelated[2];
 
     public void PassStats(CombatManager.DamageRelated Attacker, CombatManager.DamageRelated Defender)
@@ -26,6 +27,33 @@ public class getCombatInfo : MonoBehaviour
     {
         if (Stats[0].UnitReference != null && Stats[1].UnitReference != null)
         {
+            Info1[0].text = Stats[0].UnitReference.CharacterName;
+            Info1[1].text = Stats[0].UnitReference.CombatParameters.EquipedWeapon.Name;
+            Info1[2].text = Stats[0].HP.ToString();
+            Info1[3].text = Stats[0].UnitReference.Stats[0].staticValue.ToString();
+            Info1[4].text = Stats[0].Damage.ToString();
+            Info1[5].text = Stats[0].HitChance.ToString();
+            Info1[6].text = Stats[0].CritChance.ToString();
+            if (Stats[0].AttackCount > 1) { Info1[7].text = "x" + Stats[0].AttackCount; } else { Info1[7].text = ""; }
+
+            Info2[0].text = Stats[1].UnitReference.CharacterName;
+            Info2[1].text = Stats[1].UnitReference.CombatParameters.EquipedWeapon.Name;
+            Info2[2].text = Stats[1].HP.ToString();
+            Info2[3].text = Stats[1].UnitReference.Stats[0].staticValue.ToString();
+            Info2[4].text = Stats[1].Damage.ToString();
+            Info2[5].text = Stats[1].HitChance.ToString();
+            Info2[6].text = Stats[1].CritChance.ToString();
+            if (Stats[1].AttackCount > 1) { Info2[7].text = "x" + Stats[1].AttackCount; } else { Info2[7].text = ""; }
+
+
+            float percent = (float)Stats[0].HP / Stats[0].UnitReference.Stats[0].staticValue;
+            Bar1.rectTransform.localScale = new Vector3(1, percent, 1);
+
+            percent = (float)Stats[1].HP / Stats[1].UnitReference.Stats[0].staticValue;
+            Bar2.rectTransform.localScale = new Vector3(1, percent, 1);
+
+
+            /*
             Info[0].text =  Stats[0].UnitReference.CharacterName +
                             "\n" + Stats[0].UnitReference.CombatParameters.EquipedWeapon.Name +
                             "\n" +
@@ -46,6 +74,7 @@ public class getCombatInfo : MonoBehaviour
                             "\n" + Stats[1].UnitReference.CharacterName;
             if (Stats[1].AttackCount > 1) { Info[3].text = "x" + Stats[1].AttackCount; }
             else { Info[3].text = ""; }
+            */
         }
     }
 }
